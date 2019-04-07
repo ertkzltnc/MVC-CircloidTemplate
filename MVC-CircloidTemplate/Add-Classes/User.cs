99e883a39a60eadc31b52a3b490374c30c91f70d
ErtugrulKiziltunc <ertkiziltunc@gmail.com>
2019-04-07 23:09:12 +0200

    MembershipAddUser

---
+++ MVC-CircloidTemplate/Add-Classes/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CircloidTemplate.Add_Classes
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PasswordQuestion { get; set; }
+        public string PasswordAnswer { get; set; }
+    }
+}
---
+++ MVC-CircloidTemplate/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MVC_CircloidTemplate.Add_Classes;
+
+namespace MVC_CircloidTemplate.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+            ViewBag.UserSelected = "selected";
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            MembershipUserCollection users = Membership.GetAllUsers();
+            return View(users);
+
+        }
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        public ActionResult AddUser(User u)
+        {
+            Membership.CreateUser(u.UserName, u.Password, u.Email, u.PasswordQuestion, u.PasswordAnswer, true, out MembershipCreateStatus status);
+
+            string mesaj = "";
+            switch (status)
+            {
+                case MembershipCreateStatus.InvalidUserName:
+                    mesaj = "Geçersiz kullanıcı adı";
+                    break;
+                case MembershipCreateStatus.InvalidPassword:
+                    mesaj = "Geçersiz parola";
+                    break;
+                case MembershipCreateStatus.InvalidQuestion:
+                    mesaj = "Geçersiz gizli soru";
+                    break;
+                case MembershipCreateStatus.InvalidAnswer:
+                    mesaj = "Geçersiz gizli cevap";
+                    break;
+                case MembershipCreateStatus.InvalidEmail:
+                    mesaj = "Geçersiz email adresi";
+                    break;
+                case MembershipCreateStatus.DuplicateUserName:
+                    mesaj = "Kullanılmış kullanıcı adı";
+                    break;
+                case MembershipCreateStatus.DuplicateEmail:
+                    mesaj = "Kullanılmış email adresi girildi";
+                    break;
+                case MembershipCreateStatus.UserRejected:
+                    mesaj = "Kullanıcı engel hatası";
+                    break;
+                case MembershipCreateStatus.InvalidProviderUserKey:
+                    mesaj = "Geçersiz kullanıcı key hatası";
+                    break;
+                case MembershipCreateStatus.DuplicateProviderUserKey:
+                    mesaj = " Kullanılmış kullanıcı key hatası";
+                    break;
+                case MembershipCreateStatus.ProviderError:
+                    mesaj = "Üye yönetimi sağlayıcı hatası";
+                    break;
+                case MembershipCreateStatus.Success:
+                    break;
+                default:
+                    break;
+            }
+            ViewBag.Mesaj = mesaj;
+
+            if (status == MembershipCreateStatus.Success)
+                return RedirectToAction("Index");
+            else
+                return View();
+        }
+        [HttpPost]
+        public void DeleteUser(string id)
+        {
+            Membership.DeleteUser(id, deleteAllRelatedData: true);
+        }
+    }
+}
