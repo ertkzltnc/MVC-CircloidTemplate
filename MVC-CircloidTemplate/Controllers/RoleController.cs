12210af8f66d6c26dccd194454633ad0fc9b3f3c
ErtugrulKiziltunc <ertkiziltunc@gmail.com>
2019-04-08 14:29:12 +0300

    AddRole and DeleteRole

---
+++ MVC-CircloidTemplate/Controllers/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC_CircloidTemplate.Controllers
+{
+    public class RoleController : Controller
+    {
+        public RoleController()
+        {
+            ViewBag.RoleSelected = "selected";
+        }
+        // GET: Role
+        public ActionResult Index()
+        {
+            List<string> rolesList= Roles.GetAllRoles().ToList();
+            return View(rolesList);
+        }
+        public ActionResult AddRole()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult AddRole(string RoleName)
+        {
+            Roles.CreateRole(RoleName);
+            return RedirectToAction("Index");
+        }
+
+       
+        [HttpPost]
+        public void DeleteRole(string RoleName)
+        {
+            Roles.DeleteRole(RoleName);
+        }
+    }
+}
